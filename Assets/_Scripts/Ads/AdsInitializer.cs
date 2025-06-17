b70301a54bac2cc08d33f76b7ebf04ce143b2f1e
AnshikkaSaini <110725511+AnshikkaSaini@users.noreply.github.com>
2025-06-17 11:20:22 +0100

    Chore: Implemented Interstitial Ads

--- Assets/_Scripts/Ads/AdsInitializer.cs
+++ Assets/_Scripts/Ads/AdsInitializer.cs
@@ -1,39 +1,33 @@
 using UnityEngine;
 using UnityEngine.Advertisements;
- 
+
 public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
 {
-    [SerializeField] string _androidGameId;
-    [SerializeField] string _iOSGameId;
-    [SerializeField] bool _testMode = true;
+    [SerializeField] private string _androidGameId;
+    [SerializeField] private string _iOSGameId;
+    [SerializeField] private bool _testMode = true;
+
     private string _gameId;
- 
+
     void Awake()
     {
-        if (Advertisement.isInitialized)
+        if (!Advertisement.isInitialized)
         {
-            return;
-        }
+            _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+                ? _iOSGameId
+                : _androidGameId;
 
-        InitializeAds();
-    }
- 
-    public void InitializeAds()
-    {
-        _gameId = (Application.platform == 
-                                            RuntimePlatform.IPhonePlayer) 
-                                            ? _iOSGameId : _androidGameId;
-        Advertisement.Initialize(_gameId,_testMode, this);
-        
+            Advertisement.Initialize(_gameId, _testMode, this);
+        }
     }
 
     public void OnInitializationComplete()
     {
         Debug.Log("Unity Ads initialization complete.");
     }
- 
+
     public void OnInitializationFailed(UnityAdsInitializationError error, string message)
     {
-        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+        Debug.LogError($"Unity Ads Initialization Failed: {error} - {message}");
     }
 }
--- Assets/_Scripts/Ads/IntersitialAd.cs
+++ Assets/_Scripts/Ads/IntersitialAd.cs
@@ -3,56 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class IntersitialAd : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+public class InterstitialAd : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
 {
+    [SerializeField] private string _androidAdUnit = "Interstitial_Android";
+    [SerializeField] private string _iOSAdUnit = "Interstitial_iOS";
+    private string _adUnitID;
+
+    void Awake()
+    {
+#if UNITY_IOS
+        _adUnitID = _iOSAdUnit;
+#elif UNITY_ANDROID
+        _adUnitID = _androidAdUnit;
+#endif
+        LoadAd();
+    }
+
+    public void LoadAd()
+    {
+        if (Advertisement.isInitialized)
+        {
+            Advertisement.Load(_adUnitID, this);
+        }
+    }
+
+    public void ShowAd()
+    {
+            Advertisement.Show(_adUnitID, this);
+    }
+
+    // --- Load Callbacks ---
     public void OnUnityAdsAdLoaded(string placementId)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Ad Loaded: " + placementId);
+        // Optional: Auto-show ad once loaded
+         ShowAd();
     }
 
     public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
     {
-        throw new System.NotImplementedException();
+        Debug.LogError($"Failed to load Ad Unit {placementId}: {error.ToString()} - {message}");
+    }
+
+    // --- Show Callbacks ---
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        if (placementId.Equals(_adUnitID) && showCompletionState == UnityAdsShowCompletionState.COMPLETED)
+        {
+            Debug.Log("Ad Completed Successfully");
+            // Optionally: Load another ad for next time
+            LoadAd();
+        }
     }
 
     public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
     {
-        throw new System.NotImplementedException();
+        Debug.LogError($"Error showing Ad Unit {placementId}: {error.ToString()} - {message}");
     }
 
     public void OnUnityAdsShowStart(string placementId)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Ad Started");
     }
 
     public void OnUnityAdsShowClick(string placementId)
     {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
-    {
-        throw new System.NotImplementedException();
+        Debug.Log("Ad Clicked");
     }
 }
-
-[SerializeField] string _androidAdUnitId = "Interstitial_Android";
-
-    [SerializeField] string _iOSAdUnitId = "Interstitial_iOS";
-    string _adUnitId;
-
-    void Awake()
-    {
-        // Get the Ad Unit ID for the current platform:
-        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer) ?
-            _iOSAdUnitId  _androidAdUnitId;
-        UNITY_IOS
-            _adUnitID = _iOSAdUnit;
-#elif UNITY_ANDROID
-        _adUnitID = _androidAdUnit;
-#endif
-
-    }
-   
-}
-
